7d428ff524a90f242a63d38b4c178c231570833d
AmetDev <ametsejdaliev@gmail.com>
2024-01-01 23:07:17 +0300

    header

---
+++ client/my-store/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Intro/Header/Header'
+import styles from './Home.module.scss'
+const Home = () => {
+	return (
+		<div className={styles.home}>
+			<Header />
+		</div>
+	)
+}
+
+export default Home
