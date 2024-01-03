b851d7a4b73bf2395205cb041e7b5cf50e195584
AmetDev <ametsejdaliev@gmail.com>
2024-01-03 19:38:18 +0300

    block-done

--- client/my-store/src/components/Intro/Intro.jsx
+++ client/my-store/src/components/Intro/Intro.jsx
@@ -1,10 +1,12 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import chexol from '../../assets/icons/intro/chexol.png'
 import chexol1 from '../../assets/icons/intro/chexol1.png'
 import constructor from '../../assets/icons/intro/constructor.png'
 import fly1 from '../../assets/icons/intro/fly1.png'
 
+import polygons from '../../assets/icons/intro/polygons.svg'
 import styles from './Intro.module.scss'
+import IntroLink from './IntroLinks/IntroLink'
 const Intro = () => {
 	const [currentElement, setCurrentElement] = useState(0)
 	const arraycards = [
@@ -44,30 +46,70 @@ const Intro = () => {
 		return (
 			<div
 				className={styles.wrapperBlock}
+				style={{
+					background: `url(${array[currentElement].element.linkImage})`,
+					transition: 'margin-right 2s ease-in-out',
+					width: '100%',
+					height: '100%',
+				}}
 				key={array[currentElement].element.title}
 			>
 				<div className={styles.BlockUnderTitle}>Спецпредложение</div>
-				<div>{array[currentElement].element.title}</div>
-				<span>{array[currentElement].element.price} Р</span>
-				<img
-					className={styles.BlockImg}
-					src={array[currentElement].element.linkImage}
-					alt={array[currentElement].element.title}
-				/>
+				<p className={styles.BlockTitle}>
+					{array[currentElement].element.title}
+				</p>
+				<div className={styles.BlockWrapp}>
+					От&nbsp;{' '}
+					<div
+						style={{
+							color: '#424242',
+							fontSize: '42px',
+							fontStyle: 'normal',
+							fontWeight: 250,
+							lineHeight: '92.8%' /* 35.264px */,
+							textTransform: 'uppercase',
+						}}
+					>
+						{array[currentElement].element.price.toString()}₽
+					</div>
+				</div>
 			</div>
 		)
 	}
+	useEffect(() => {
+		let count = 0
+		function loopCounter() {
+			setInterval(() => {
+				console.log(count)
+				setCurrentElement(count)
+				ref.current.map(el => {
+					el.style.backgroundColor = '#dfe3f0	'
+				})
+				ref.current[count].style.backgroundColor = '#011a6c'
+				count = (count + 1) % 4
+			}, 5000)
+		}
+
+		loopCounter()
+	}, [])
 	return (
 		<div className={styles.intro_wrapper}>
-			<div className={styles.intro_wrapper_paraph}>
-				<span>Создайте&nbsp;</span> индивидуальный
-				<span>дизайн&nbsp;</span> для своего
-				<span>&nbsp;чехла</span>
+			<div className={styles.wrapperFirst}>
+				<div className={styles.intro_wrapper_paraph}>
+					<span>Создайте&nbsp;</span> индивидуальный
+					<span>дизайн&nbsp;</span> для своего
+					<span>&nbsp;чехла</span>
+				</div>
+				<div className={styles.IntroWrappersParagraph}>
+					<p>Или выберите готовый шаблон</p>
+					<img src={polygons} alt='' />
+				</div>
 			</div>
-			<div className={styles.block_fly}>
-				<Block currentElement={currentElement} />
-				<div>
-					<div>
+
+			<div className={styles.wrapperSecond}>
+				<div className={styles.block_fly}>
+					<Block currentElement={currentElement} />
+					<div className={styles.btns}>
 						{array.map((el, index) => {
 							return (
 								<button
@@ -85,6 +127,10 @@ const Intro = () => {
 							)
 						})}
 					</div>
+					<div></div>
+				</div>
+				<div className={styles.IntroLinkWrapperLast}>
+					<IntroLink />
 				</div>
 			</div>
 		</div>
---
+++ client/my-store/src/components/Intro/IntroLinks/IntroLink.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import facebook from '../../../assets/icons/intro/facebook.svg'
+import instagram from '../../../assets/icons/intro/instagram.svg'
+import mail from '../../../assets/icons/intro/mail.svg'
+import vk from '../../../assets/icons/intro/vk.svg'
+import styles from '../IntroLinks/IntroLink.module.scss'
+
+const IntroLink = () => {
+	const linksData = [
+		{
+			link: vk,
+			linkHref: 'https://vk.com/justzero09',
+		},
+		{
+			link: facebook,
+			linkHref: 'https://vk.com/justzero09',
+		},
+		{
+			link: instagram,
+			linkHref: 'https://vk.com/justzero09',
+		},
+		{
+			link: mail,
+			linkHref: 'https://vk.com/justzero09',
+		},
+	]
+	return (
+		<div className={styles.IntroIconLinks}>
+			{linksData.map(link => {
+				return (
+					<a href={link.linkHref}>
+						<img src={link.link} alt='' srcset='' />
+					</a>
+				)
+			})}
+		</div>
+	)
+}
+
+export default IntroLink
